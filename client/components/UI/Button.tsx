599643bdd6bc4f2b27cafef059bfd4ed8f641e5d
Elias Kaleta <eliaskaleta@gmail.com>
2024-09-13 11:56:03 +1200

    f/47 COMPLETE: Add daisyUI custom theme. Add prebuilt UI components

---
+++ client/components/UI/Button.tsx
@@ -0,0 +1,22 @@
+import { HtmlHTMLAttributes } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+type ButtonProps = HtmlHTMLAttributes<HTMLButtonElement>
+
+//** To Call: <Button onClick='...'> Button Name <Button/> */
+
+// Alternate styles, in YOUR component when you call, add className='btn-**any-of-the-following**'
+// neutral, primary, secondary, accent, ghost, link
+// eg.  <Button className='btn-primary'> Primary Button <Button/>
+
+// More button info and styles: https://daisyui.com/components/button/
+
+function Button({ children, className, ...rest }: ButtonProps) {
+  return (
+    <button className={twMerge('btn', className)} {...rest}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
