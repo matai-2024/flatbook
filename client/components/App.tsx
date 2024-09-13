e925ff359fc942f9e74861612902f722d980aadb
Elias Kaleta <eliaskaleta@gmail.com>
2024-09-14 10:39:25 +1200

    Tidy up and solve PR comments

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,20 +1,10 @@
-// import { useFlats } from '../hooks/useFlats.ts'
 import Nav from './Nav/Nav.tsx'
 import AppLayout from './AppLayout'
-import { useAuth0 } from '@auth0/auth0-react'
-import { useEffect, useState } from 'react'
 
 function App() {
-  // const { data } = useFlats()
-  const { isAuthenticated } = useAuth0()
-  const [navState, setNavState] = useState(false)
-
-  useEffect(() => {
-    isAuthenticated === false ? setNavState(false) : setNavState(true)
-  }, [isAuthenticated])
   return (
     <>
-      <Nav navState={navState} />
+      <Nav />
       <AppLayout />
     </>
   )
--- client/components/Nav/Nav.tsx
+++ client/components/Nav/Nav.tsx
@@ -2,19 +2,17 @@ import LoginButton from '../LoginButton'
 import logo from '../../assets/flatbook-logo.png'
 import LogoutButton from '../LogoutButton'
 import Button from '../UI/Button'
+import { useAuth0 } from '@auth0/auth0-react'
 
-interface Props {
-  navState: boolean
-}
-
-function Nav(props: Props) {
+function Nav() {
+  const { isAuthenticated } = useAuth0()
   return (
     <div className="bg-base100 navbar mb-5 shadow-md">
       <div className="ml-8 flex-1">
         <img src={logo} alt="logo" className=" w- btn btn-ghost h-28" />
       </div>
       <div className="mr-20">
-        {props.navState === false ? (
+        {isAuthenticated === false ? (
           <LoginButton />
         ) : (
           <div>
