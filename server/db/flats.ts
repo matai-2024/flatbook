aa8838790d18b2162d389c31951c38b2f60b690a
Ryan Zhao <yimian.zhao@gmail.com>
2024-09-13 12:09:44 +1200

    Fix a missing fruit

--- server/db/flats.ts
+++ server/db/flats.ts
@@ -2,13 +2,13 @@ import db from './connection.ts'
 import { Flat, FlatData } from '../../models/flat.ts'
 
 export async function getAllFlats() {
-  const fruit = await db('flats').select()
-  return fruit as Flat[]
+  const flat = await db('flats').select()
+  return flat as Flat[]
 }
 
 export async function getFlatById(id: number | string) {
-  const fruit = await db('flats').select().first().where({ id })
-  return fruit as Flat
+  const flat = await db('flats').select().first().where({ id })
+  return flat as Flat
 }
 
 export async function addFlat(data: FlatData) {
