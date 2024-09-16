d9b2c1be8e1119a0271b64430ff48c9220fd59c3
POR7ALa <thatsair@gmail.com>
2024-09-16 15:39:15 +1200

    fix: id -> flatId

--- client/hooks/useFetchAnnouncements.ts
+++ client/hooks/useFetchAnnouncements.ts
@@ -2,11 +2,11 @@ import { useQuery } from '@tanstack/react-query'
 import request from 'superagent'
 import { Announcement } from '../../types/Announcement'
 
-export default function useFetchChores(id: string) {
+export default function useFetchAnnouncements(flatId: string) {
   return useQuery({
     queryKey: ['announcements'],
     queryFn: async () => {
-      const res = await request.get(`/api/v1/announcements/${id}`)
+      const res = await request.get(`/api/v1/announcements/${flatId}`)
       return res.body as Announcement[]
     },
   })
