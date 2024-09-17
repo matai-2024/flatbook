3520016c482269c7580e288e134c39ff5c9239f8
Ryan Zhao <yimian.zhao@gmail.com>
2024-09-17 17:56:54 +1200

    Merge main

--- client/apis/flats.ts
+++ client/apis/flats.ts
@@ -1,13 +1,24 @@
 import request from 'superagent'
+import { Flat } from '../../types/Flat'
 import { FlatData } from '../../types/Flat'
 
-const rootUrl = '/api/v1/flats'
+const baseUrl = '/api/v1/flats/flats'
+
+export async function getFlatById(
+  token: string,
+  flatId: number,
+): Promise<Flat> {
+  const res = await request
+    .get(`${baseUrl}/${flatId}`)
+    .set('Authorization', `Bearer ${token}`)
+  return res.body as Flat
+}
 
 export async function addFlat(newFlat: FlatData, token: string) {
   const date = new Date(Date.now()).toISOString()
 
   const res = await request
-    .post(rootUrl)
+    .post(baseUrl)
     .set('Authorization', `Bearer ${token}`)
     .send({ ...newFlat, createdAt: date })
   return res.body as number
