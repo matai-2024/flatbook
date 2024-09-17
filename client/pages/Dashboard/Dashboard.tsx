d65a47fbcfc51f8eec4383aa98b757735addb6ee
Elias Kaleta <eliaskaleta@gmail.com>
2024-09-18 11:15:16 +1200

    Change logic for dashboard to flat sign up redirect

--- client/pages/Dashboard/Dashboard.tsx
+++ client/pages/Dashboard/Dashboard.tsx
@@ -8,7 +8,7 @@ import ShopList from '../../components/ShoppingList/ShopList'
 import Bills from '../../components/Bills/Bills'
 import { useGetFlatByAuthId } from '../../hooks/useFlats'
 import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import Button from '../../components/UI/Button'
 
 /* Todo: To be replaced by real widgets */
 // function WidgetExample() {
@@ -83,16 +83,6 @@ function Dashboard() {
 
   const navigate = useNavigate()
 
-  //If flatID is undefined, null or the default value (0), redirect to flat_setup
-  useEffect(() => {
-    if (isLoading) {
-      return
-    }
-    if ((flatId !== undefined && flatId === null) || flatId.flatId === 0) {
-      navigate(`/flat_setup`, { replace: true })
-    }
-  }, [flatId, isLoading, navigate])
-
   if (isLoading) {
     return <>Loading</>
   }
@@ -100,49 +90,67 @@ function Dashboard() {
   if (isError) {
     return <ErrorPage />
   }
-  return (
-    <>
-      <div className="container flex flex-row">
-        <div className="container static my-0 max-w-96 bg-[#d9d9d9] px-5 py-5">
-          <Sidebar />
-        </div>
+
+  if ((flatId !== undefined && flatId === null) || flatId.flatId === 0) {
+    // The user doesn't have a flat yet
+    return (
+      <div className="container">
+        <p>Looks like you have not had a flat.</p>
+        <Button
+          onClick={() => {
+            navigate(`/flat_setup`, { replace: true })
+          }}
+        >
+          Next
+        </Button>
+      </div>
+    )
+  } else {
+    return (
+      <>
         <div className="container flex flex-row">
-          <div className="container flex flex-col">
-            <div className="max-auto container mb-5 min-w-96 px-5">
-              <h3 className="py-5 text-center text-2xl font-bold">
-                Announcements
-              </h3>
-              <CreateAnnouncement flatId={flatId} />
-              <FlatAnnouncements flatId={flatId} />
-            </div>
-            <div className="max-auto container mb-5 min-w-96 px-5">
-              <h3 className="py-5 text-center text-2xl font-bold">Events</h3>
-              <ChatsExample /> {/* Todo: replace with real Events component */}
-            </div>
+          <div className="container static my-0 max-w-96 bg-[#d9d9d9] px-5 py-5">
+            <Sidebar />
           </div>
-          <div className="container flex flex-col">
-            <div className="max-auto container mb-5">
-              <h3 className="py-5 text-center text-2xl font-bold">Chores</h3>
-              <div className="container flex flex-row space-x-1">
-                <CreateChore flatId={flatId} />
-                <ChoresList flatId={flatId} />
+          <div className="container flex flex-row">
+            <div className="container flex flex-col">
+              <div className="max-auto container mb-5 min-w-96 px-5">
+                <h3 className="py-5 text-center text-2xl font-bold">
+                  Announcements
+                </h3>
+                <CreateAnnouncement flatId={flatId} />
+                <FlatAnnouncements flatId={flatId} />
+              </div>
+              <div className="max-auto container mb-5 min-w-96 px-5">
+                <h3 className="py-5 text-center text-2xl font-bold">Events</h3>
+                <ChatsExample />{' '}
+                {/* Todo: replace with real Events component */}
               </div>
             </div>
-            <div className="max-auto container mb-5">
-              <h3 className="py-5 text-center text-2xl font-bold">
-                Shopping List
-              </h3>
-              <ShopList flatId={flatId} />
-            </div>
-            <div className="max-auto container mb-5">
-              <h3 className="py-5 text-center text-2xl font-bold">Bills</h3>
-              <Bills /> {/* Todo: replace with real Bills component */}
+            <div className="container flex flex-col">
+              <div className="max-auto container mb-5">
+                <h3 className="py-5 text-center text-2xl font-bold">Chores</h3>
+                <div className="container flex flex-row space-x-1">
+                  <CreateChore flatId={flatId} />
+                  <ChoresList flatId={flatId} />
+                </div>
+              </div>
+              <div className="max-auto container mb-5">
+                <h3 className="py-5 text-center text-2xl font-bold">
+                  Shopping List
+                </h3>
+                <ShopList flatId={flatId} />
+              </div>
+              <div className="max-auto container mb-5">
+                <h3 className="py-5 text-center text-2xl font-bold">Bills</h3>
+                <Bills /> {/* Todo: replace with real Bills component */}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default Dashboard
